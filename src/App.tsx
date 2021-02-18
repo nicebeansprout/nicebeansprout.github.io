80ce098c1717f9bbbe75c8f3e11f00fdacb7e8e4
nicebeansprout <nicebeansprout@gmail.com>
2021-02-18 02:20:44 -0500

    switched to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import './App.scss';
 import Homepage from './Homepage';
 import Cats from './Cats';
@@ -8,12 +8,12 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className='app-root'>
-				<Router>
+				<HashRouter>
 					<Switch>
 						<Route exact path='/' component={Homepage} />
 						<Route exact path='/cats' component={Cats} />
 					</Switch>
-				</Router>
+				</HashRouter>
 			</div>
 		);
 	}
